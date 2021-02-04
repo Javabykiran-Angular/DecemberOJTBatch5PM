551f727e2a186e9ad97067cb5b9af256a835137e
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2021-02-04 18:47:30 +0530

    Day2

---
+++ Typescript/Day2/demo1.js
@@ -0,0 +1,17 @@
+// System.out.println("Hello World");
+/*
+
+*/
+console.log("Hello World");
+console.log('Welcome to Angular Session');
+// datatype
+// number(int,float,double,long,short), string(char,string),boolean(true/false),
+// void  ,any(number,string,boolean)
+//misec undefined  & null => datatype as well as value
+var a1;
+var a_2;
+var a2 = 10; //Forward declration & defination
+// int a=20;
+var a3;
+//int a;
+console.log("Value of a3 is " + a3);
