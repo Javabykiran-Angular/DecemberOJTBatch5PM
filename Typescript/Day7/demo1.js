6ae73fda0e76ec90fc67895a855723aaccfc0fce
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2021-02-15 18:25:11 +0530

    Day7

---
+++ Typescript/Day7/demo1.js
@@ -0,0 +1,124 @@
+//Array of json Object
+var arrobj = [
+    {
+        fname: "Sumit",
+        lname: "Raokhande",
+        id: 1
+    },
+    {
+        fname: "Kiran",
+        lname: "Raokhande",
+        id: 2
+    },
+    {
+        fname: "Spruha",
+        lname: "Raokhande",
+        id: 3
+    }
+];
+// console.log(`
+// -------------------------------
+//     First Name :: ${arrobj[0].fname}
+//     Last Name :: ${arrobj[0].lname}
+//     ID       :: ${arrobj[0].id}
+// `);
+// for (let i = 0; i < arrobj.length; i++) {
+//     console.log(`
+// -------------------------------
+//     First Name :: ${arrobj[i].fname}
+//     Last Name :: ${arrobj[i].lname}
+//     ID       :: ${arrobj[i].id}
+// `);
+// }
+var arrobj1 = [
+    {
+        name: "Samsung",
+        price: 10000,
+        quantity: 1,
+        description: {
+            specification: "Technical",
+            design: 'Good',
+            Tech: {
+                processor: "Snapdragon 765",
+                battery: "4000mah",
+                ram: "12GB",
+                modelno: 7570
+            }
+        }
+    },
+    {
+        name: "Motorolla",
+        price: 20000,
+        quantity: 1,
+        description: {
+            specification: "Technical",
+            design: 'Good',
+            Tech: {
+                processor: "Snapdragon 765",
+                battery: "4000mah",
+                ram: "12GB",
+                modelno: 7570
+            }
+        }
+    },
+    {
+        name: "One Plus",
+        price: 30000,
+        quantity: 2,
+        description: {
+            specification: "Technical",
+            design: 'Good',
+            Tech: {
+                processor: "Snapdragon 765",
+                battery: "4000mah",
+                ram: "12GB",
+                modelno: 7570
+            }
+        }
+    }
+];
+// for (let i = 0; i < arrobj1.length; i++) {
+//         console.log(`
+//     -------------------------------
+//         Product Name :: ${arrobj1[i].name}
+//         Product Price :: ${arrobj1[i].price}
+//         Product Quantity:: ${arrobj1[i].quantity}
+//         Description::  
+//                     1 Specification: ${arrobj1[i].description.specification}
+//                     2 Design       : ${arrobj1[i].description.design} 
+//                     3 Tech         :
+//                             processor: ${arrobj1[i].description.Tech.processor}  
+//                             Battery: ${arrobj1[i].description.Tech.battery}
+//                             Ram: ${arrobj1[i].description.Tech.ram}
+//                             Model no: ${arrobj1[i].description.Tech.modelno}
+//     `);
+//     }
+var indusJson = {
+    status: "",
+    data: [
+        {
+            fname: "Sumit",
+            lname: "Raokhande",
+            id: 1
+        },
+        {
+            fname: "Kiran",
+            lname: "Raokhande",
+            id: 2
+        },
+        {
+            fname: "Spruha",
+            lname: "Raokhande",
+            id: 3
+        }
+    ],
+    error: "Error Occured"
+};
+if (indusJson.status == "Success") {
+    for (var i = 0; i < indusJson.data.length; i++) {
+        console.log("\n                First name: " + indusJson.data[i].fname + "\n                Last name: " + indusJson.data[i].lname + "\n                Id      : " + indusJson.data[i].id + "\n        ");
+    }
+}
+else {
+    console.log(indusJson.error);
+}
