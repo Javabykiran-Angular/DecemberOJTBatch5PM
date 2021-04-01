fe8b99932da29374e4d30d0bf180200b60dc9ffc
Javabykiran-Angular <71135924+Javabykiran-Angular@users.noreply.github.com>
2021-04-01 18:37:47 +0530

    Day21

---
+++ Angular/Day21/tdfdemo/tdfdemo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tdfdemo',
+  templateUrl: './tdfdemo.component.html',
+  styleUrls: ['./tdfdemo.component.css']
+})
+export class TdfdemoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
